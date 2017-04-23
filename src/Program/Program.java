3e495d361e588273239164291e302a59d80b1641
Yrjo Koyen <yrjo@live.be>
2017-04-23 14:54:29 +0200

    Add constructor for Program class

--- src/Program/Program.java
+++ src/Program/Program.java
@@ -4,6 +4,30 @@ import java.util.List;
 
 public class Program {
 	
+	public Program(List<Function> functions, Statement main) {
+		setFunctions(functions);
+		setMainStatement(main);
+	}
+	
+	
+	public List<Function> getFunctions() {
+		return functions;
+	}
+	
+	private void setFunctions(List<Function> functions) {
+		this.functions = functions;
+	}
+	
 	private List<Function> functions;
+	
+	public Statement getMainStatement() {
+		return this.mainStatement;
+	}
+	
+	private void setMainStatement(Statement main) {
+		this.mainStatement = main;
+	}
+	
+	private Statement mainStatement;
 
 }
