2523e7e72b45a3a65c5787d21ce95c7a979acf81
Yrjo Koyen <yrjo@live.be>
2017-04-23 14:54:16 +0200

    Add constructor for Function class

--- src/Program/Function.java
+++ src/Program/Function.java
@@ -1,5 +1,30 @@
 package Program;
 
+import asteroids.part3.programs.SourceLocation;
+
 public class Function {
 
+	public Function(String functionName, Statement body, SourceLocation sourceLocation) {
+		this.name = functionName;
+		this.body = body;
+		this.sourceLocation = sourceLocation;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	private String name;
+	
+	public Statement getBody() {
+		return this.body;
+	}
+
+	private Statement body;
+	
+	public SourceLocation getSourceLocation() {
+		return this.sourceLocation;
+	}
+	
+	private SourceLocation sourceLocation;
 }
