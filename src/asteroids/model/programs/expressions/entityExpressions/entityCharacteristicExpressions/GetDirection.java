c090671dbb0e91416b76b783956911bc6df440a8
Bo Kleynen <kleynenbo@gmail.com>
2017-05-06 16:23:45 +0200

    updated GetDirection
    this class is now more in line with the other classes regarding entity characteristics

--- src/asteroids/model/programs/expressions/entityExpressions/entityCharacteristicExpressions/GetDirection.java
+++ src/asteroids/model/programs/expressions/entityExpressions/entityCharacteristicExpressions/GetDirection.java
@@ -1,14 +1,19 @@
 package asteroids.model.programs.expressions.entityExpressions.entityCharacteristicExpressions;
 
-import asteroids.model.programs.expressions.Expression;
+import asteroids.model.Ship;
+import asteroids.model.programs.expressions.entityExpressions.SelfExpression;
 
 /**
  * Created by Bo on 28/04/2017.
  */
-public class GetDirection extends Expression<Double> {
+public class GetDirection extends EntityCharacteristicExpression {
+
+    public GetDirection() {
+        super(new SelfExpression());
+    }
 
     @Override
     public Double getValue() {
-        return getStatement().getProgram().getShip().getOrientation();
+        return ((Ship) getEntity()).getOrientation();
     }
 }
