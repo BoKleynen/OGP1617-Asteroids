b6f9f732b81bf9609963fa098ff9da6681734217
Yrjo Koyen <yrjo@live.be>
2017-05-07 14:46:17 +0200

    Add tests for While class

---
+++ tests/testWhileLoop.java
@@ -0,0 +1,72 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import asteroids.model.Program;
+import asteroids.model.programs.expressions.Expression;
+import asteroids.model.programs.expressions.ReadVariableExpression;
+import asteroids.model.programs.expressions.binaryExpressions.compareExpressions.LessThan;
+import asteroids.model.programs.expressions.valueExpressions.ValueExpression;
+import asteroids.model.programs.function.Function;
+import asteroids.model.programs.statements.Assignment;
+import asteroids.model.programs.statements.Break;
+import asteroids.model.programs.statements.Sequence;
+import asteroids.model.programs.statements.Statement;
+import asteroids.model.programs.statements.While;
+
+public class testWhileLoop {
+	
+	Program P;
+	List<Function> F;
+	While loop;
+	Statement statement;
+	Sequence S;
+	Break B;
+	Expression condition;
+	List<Statement> statementList;
+	
+	@Before
+	public void setup() {
+		statementList = new ArrayList<>();
+		F = new ArrayList<Function>();
+		statement = new Assignment("var", new ValueExpression<Integer>(5));
+		condition = new ValueExpression<Boolean>(true);
+		B = new Break();
+	}
+	
+	@Test
+	public void testBreakOutOfWhileLoopWithSequence() {
+		statementList.add(statement);
+		statementList.add(B);
+		S = new Sequence(statementList);
+		loop = new While(condition, S);
+		P = new Program(F, loop);
+
+		loop.execute();
+	}
+	
+	@Test
+	public void testBreakOutOfWhileLoopWithSingleStatement() {
+		loop = new While(condition, B);
+		P = new Program(F, loop);
+
+		loop.execute();
+	}
+	
+	@Test
+	public void testWhileLoopWithFalseCondition() {
+		statementList.add(statement);
+		statementList.add(B);
+		S = new Sequence(statementList);
+		condition = new ValueExpression<Boolean>(false);
+		loop = new While(condition, S);
+		
+		loop.execute();
+	}
+	
+
+}
