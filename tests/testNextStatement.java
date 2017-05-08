142c29b58d4f596108f083f344210dd03f662fbe
Yrjo Koyen <yrjo@live.be>
2017-05-08 22:29:12 +0200

    Add tests
    Only While loops with breaks don't seem to work

--- tests/testNextStatement.java
+++ tests/testNextStatement.java
@@ -11,6 +11,7 @@ import asteroids.model.programs.expressions.valueExpressions.ValueExpression;
 import asteroids.model.programs.function.Function;
 import asteroids.model.programs.statements.Assignment;
 import asteroids.model.programs.statements.Break;
+import asteroids.model.programs.statements.If;
 import asteroids.model.programs.statements.Print;
 import asteroids.model.programs.statements.Sequence;
 import asteroids.model.programs.statements.Statement;
@@ -55,5 +56,84 @@ public class testNextStatement {
 		lst.add(A);
 		lst.add(P);
 		lst.add(B);
+		
+		Sequence seq = new Sequence(lst);
+		
+		assertTrue(seq.next().equals(A));
+		assertTrue(seq.next().equals(P));
+		assertTrue(seq.next().equals(B));
+		assertTrue(seq.next() == null);
+		assertTrue(seq.next() == null);
+		
+		seq.resetNext();
+		
+		assertTrue(seq.next().equals(A));
+		assertTrue(seq.next().equals(P));
+		assertTrue(seq.next().equals(B));
+		assertTrue(seq.next() == null);
+		assertTrue(seq.next() == null);
+	}
+	
+	@Test
+	public void testIfNext() {
+		Assignment A = new Assignment("Hello", new ValueExpression<Double>(5.0));
+		Print P = new Print(new ValueExpression<Double>(5.0));
+		Break B = new Break();
+		List<Statement> lst = new ArrayList<>();
+		lst.add(A);
+		lst.add(P);
+		lst.add(B);
+		
+		Sequence seq = new Sequence(lst);
+		
+		ValueExpression<Boolean> condition = new ValueExpression<Boolean>(true);
+		If ifStatement = new If(condition, seq, P);
+		
+		assertTrue(ifStatement.next().equals(A));
+		assertTrue(ifStatement.next().equals(P));
+		assertTrue(ifStatement.next().equals(B));
+		assertTrue(ifStatement.next() == null);
+		assertTrue(ifStatement.next() == null);
+		assertTrue(ifStatement.next() == null);
+		
+		condition = new ValueExpression<Boolean>(false);
+		ifStatement = new If(condition, seq, P);
+		
+		assertTrue(ifStatement.next().equals(P));
+		assertTrue(ifStatement.next() == null);
+		assertTrue(ifStatement.next() == null);
+		assertTrue(ifStatement.next() == null);
+	}
+	
+	@Test
+	public void testWhileNext() {
+		Assignment A = new Assignment("Hello", new ValueExpression<Double>(5.0));
+		Print P = new Print(new ValueExpression<Double>(5.0));
+		List<Statement> lst = new ArrayList<>();
+		lst.add(A);
+		lst.add(P);
+		
+		Sequence seqWithoutBreak = new Sequence(lst);
+		
+		// When condition remains true, the sequence will be returned in order forever
+		ValueExpression<Boolean> condition = new ValueExpression<Boolean>(true);
+		While whileStatement = new While(condition, seqWithoutBreak);
+		
+		assertTrue(whileStatement.next().equals(A));
+		assertTrue(whileStatement.next().equals(P));
+		assertTrue(whileStatement.next().equals(A));
+		assertTrue(whileStatement.next().equals(P));
+		assertTrue(whileStatement.next().equals(A));
+		assertTrue(whileStatement.next().equals(P));
+		
+		// When condition is false, null we be returned
+		condition = new ValueExpression<Boolean>(false);
+		whileStatement = new While(condition, seqWithoutBreak);
+		
+		assertTrue(whileStatement.next() == null);
+		assertTrue(whileStatement.next() == null);
+		assertTrue(whileStatement.next() == null);
+		assertTrue(whileStatement.next() == null);
+		
 	}
 }
